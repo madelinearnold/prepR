0f94788528385d7078773054b90d7fe296edf937
Madeline Arnold <madeline_arnold@berkeley.edu>
2026-01-07 10:59:22 -0800

    Converting get dashboard_varinfo to be more general and take the flag column name used for filtering as input

--- R/get_dashboard_varinfo.R
+++ R/get_dashboard_varinfo.R
@@ -1,14 +1,22 @@
 #' Slim Varinfo for Export for Dashboard Display
 #'
-#' Filters the varinfo to only include variables marked for dashboard display
-#' and selects a specific subset of columns required by the dashboard.
+#' Filters the varinfo to only include variables marked for a specific
+#' display and selects a specific subset of columns required by the dashboard.
 #'
 #' @param df The varinfo data frame.
+#' @param display_col String. The name of the boolean flag column to filter on.
+#' Defaults to "DASH_DISPLAY".
 #' @return A filtered and column-trimmed tibble.
-#' @importFrom dplyr filter select any_of
+#' @importFrom dplyr filter select any_of sym
 #' @family varinfo prep functions
 #' @export
-get_dashboard_varinfo <- function(df) {
+#' @examples
+#' # Export for Program Dashboard
+#' dash_export <- get_dashboard_varinfo(varinfo, "DASH_DISPLAY")
+#'
+#' # Export for General Dashboards
+#' general_viz_export <- get_dashboard_varinfo(varinfo, "GENERAL_VIZ_DISPLAY")
+get_dashboard_varinfo <- function(df, display_col = "DASH_DISPLAY") {
   target_cols <- c(
     "ITEM_NAME", "ITEM_TYPE", "ITEM_PRESENTATION_TYPE", "ITEM_SECTION",
     "ITEM_PARENT_ID", "ITEM_STEM", "ITEM_MEMBER", "SCALE_OPTIONS",
@@ -16,6 +24,6 @@ get_dashboard_varinfo <- function(df) {
   )
 
   df |>
-    filter(as.logical(DASH_DISPLAY)) |> # converts "TRUE" string --> TRUE
+    filter(as.logical(!!sym(display_col))) |> # converts "TRUE" string --> TRUE
     select(any_of(target_cols))
 }
